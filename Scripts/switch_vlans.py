f4139c0b2a2b70b37873eaa6f1f585b546dc8049
rallepe <rasmus.perssonps4@gmail.com>
2026-02-18 22:16:23 +0100

    switch_vlans_for_loop

--- Scripts/switch_vlans.py
+++ Scripts/switch_vlans.py
@@ -16,9 +16,9 @@ if password:
 tn.write(b"enable\n")
 tn.write(b"cisco\n")
 tn.write(b"conf t\n")
-for i in range(1, 100):
-    tn.write(f"vlan {i}\n"
-    tn.write(f"name Python_VLAN_{i}\n"
+for i in range(1, 101):
+    tn.write(f"vlan {i}\n")
+    tn.write(f"name Python_VLAN_{i}\n")
 
 tn.write(b"end\n")
 tn.write(b"exit\n") 
