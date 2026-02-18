0d0332c36515d34ec563f840333372316015da80
rallepe <rasmus.perssonps4@gmail.com>
2026-02-18 23:42:08 +0100

    flera switchar

--- Scripts/switch_vlans.py
+++ Scripts/switch_vlans.py
@@ -1,27 +1,28 @@
 import getpass
 import telnetlib
 
-HOST = "192.168.255.101"
+f = open("myswitches")
+
+
 user = input("Enter your telnet username: ")
 password = getpass.getpass()
 
-tn = telnetlib.Telnet(HOST)
 
-tn.read_until(b"Username: ")
-tn.write(user.encode('ascii') + b"\n")
-if password:
-    tn.read_until(b"Password: ")
-    tn.write(password.encode('ascii') + b"\n")
-
-tn.write(b"enable\n")
-tn.write(b"cisco\n")
-tn.write(b"conf t\n")
-for i in range(1, 101):
-    tn.write(f"vlan {i}\n")
-    tn.write(f"name Python_VLAN_{i}\n")
-
-tn.write(b"end\n")
-tn.write(b"exit\n") 
+for IP in f:
+    tn = telnetlib.Telnet(IP.strip())
+    tn.read_until(b"Username: ")
+    tn.write(user.encode('ascii') + b"\n")
+    if password:
+        tn.read_until(b"Password: ")
+        tn.write(password.encode('ascii') + b"\n")
+        tn.write(b"enable\n")
+        tn.write(b"cisco\n")
+        tn.write(b"conf t\n")
+        for i in range(1, 101):
+            tn.write(f"vlan {i}\n")
+            tn.write(f"name Python_VLAN_{i}\n")
+        tn.write(b"end\n")
+        tn.write(b"exit\n") 
 
 
 print(tn.read_all().decode('ascii'))
