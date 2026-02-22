4920a0ede5190d01f4dbf09865cedf2b0e4d78e5
rallepe <rasmus.perssonps4@gmail.com>
2026-02-22 20:35:14 +0100

    Netmiko create VLAN

---
+++ Scripts/Netmiko/netmiko_create_configuration_using_a_file.py
@@ -0,0 +1,41 @@
+
+
+
+netmiko import connect_handler
+
+
+
+
+iosv_l2_s1 =  {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.255.100',
+    'username': 'david',
+    'password': 'cisco',
+    }
+
+iosv_l2_s2 =  {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.255.101',
+    'username': 'david',
+    'password': 'cisco',
+    }
+
+iosv_l2_s3 =  {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.255.102',
+    'username': 'david',
+    'password': 'cisco',
+    }
+
+with open("config") as f:
+    config_commands = f.read().splitlines()
+print(config_commands)
+
+
+all_devices = [iosv_l2_s1, iosv_l2_s2, iosv_l2_s3]
+
+
+for device in all_devices:
+    net_connect = ConnectHandler(**device)
+    output = net_connect.send_config_set(config_commands)
+    print(output)
