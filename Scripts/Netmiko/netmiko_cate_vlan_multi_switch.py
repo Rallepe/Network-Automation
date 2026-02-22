d2bbe22567d33b10261ba7cbcbd66ff949e1116d
rallepe <rasmus.perssonps4@gmail.com>
2026-02-22 19:55:59 +0100

    Netmiko create VLAN

--- Scripts/Netmiko/netmiko_cate_vlan_multi_switch.py
+++ Scripts/Netmiko/netmiko_cate_vlan_multi_switch.py
@@ -29,8 +29,7 @@ all_devices = [iosv_l2_s1, iosv_l2_s2, iosv_l2_s3]
 for device in all_devices:
     net_connect = ConnectHandler(**device)
     for n in range (2, 21):
-        print(f'Creating VLAN {n}' on device['ip'])
+        print(f'Creating VLAN {n} on device {device["ip"]}')
         config_commands = [f'vlan {n}', f'name Python_VLAN_{n}']
         output = net_connect.send_config_set(config_commands)
         print(output)
-        
